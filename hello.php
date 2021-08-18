061f939841769718c2bf4d52850cba9fe69e0599
mt8 <info@mt8.biz>
2021-08-19 07:26:25 +0900

    change for tencho words

--- hello.php
+++ hello.php
@@ -1,93 +1,76 @@
 <?php
 /**
- * @package Hello_Dolly
- * @version 1.7.2
+ * @package Hello_Tencho
+ * @version 1.0.0
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.7.2
-Author URI: http://ma.tt/
+Plugin Name: Hello Tencho
+Plugin URI: https://github.com/mt8/hello-tencho/
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words said most famously by Motohachi Tencho: Hello, Tencho. When activated you will randomly see a lyric from <cite>Hello, Tencho</cite> in the upper right of your admin screen on every page.
+Author: mt8
+Version: 1.0.0
+Author URI: https://mt8.biz/
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, take her wrap, fellas
-Dolly, never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, golly, gee, fellas
-Have a little faith in me, fellas
-Dolly, never go away
-Promise, you'll never go away
-Dolly'll never go away again";
+function hello_tencho_get_words() {
+	/** These are the words to Hello Tencho */
+	$words = "Hi Tencho, I'm Tencho and I'm always selling mangoes. 
+	Do you like red mangoes? Or do you like green mangoes? 
+	Mangoes can only be harvested in the summer. 
+	I take good care of the mangoes I harvest. 
+	Do not put the mangoes in the refrigerator right away. 
+	The ripening process is very important for mangoes. 
+	When they change color and smell sweet, it is a sign that they are fully ripe. 
+	If you suddenly put them in the refrigerator, the ripening process will stop. 
+	After eating a delicious mango, try planting the seeds. 
+	Ah, mangoes. Ah, mangoes.";
 
 	// Here we split it into lines.
-	$lyrics = explode( "\n", $lyrics );
+	$words = explode( "\n", $words );
 
 	// And then randomly choose a line.
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+	return wptexturize( $words[ mt_rand( 0, count( $words ) - 1 ) ] );
 }
 
 // This just echoes the chosen line, we'll position it later.
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
+function hello_tencho() {
+	$chosen = hello_tencho_get_words();
 	$lang   = '';
 	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
 		$lang = ' lang="en"';
 	}
 
 	printf(
-		'<p id="dolly"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
-		__( 'Quote from Hello Dolly song, by Jerry Herman:', 'hello-dolly' ),
+		'<p id="tencho"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
+		__( 'Quote from Hello Tencho wards, by Motohachi:', 'hello-tencho' ),
 		$lang,
 		$chosen
 	);
 }
 
 // Now we set that function up to execute when the admin_notices action is called.
-add_action( 'admin_notices', 'hello_dolly' );
+add_action( 'admin_notices', 'hello_tencho' );
 
 // We need some CSS to position the paragraph.
-function dolly_css() {
+function tencho_css() {
 	echo "
 	<style type='text/css'>
-	#dolly {
+	#tencho {
 		float: right;
 		padding: 5px 10px;
 		margin: 0;
 		font-size: 12px;
 		line-height: 1.6666;
 	}
-	.rtl #dolly {
+	.rtl #tencho {
 		float: left;
 	}
-	.block-editor-page #dolly {
+	.block-editor-page #tencho {
 		display: none;
 	}
 	@media screen and (max-width: 782px) {
-		#dolly,
-		.rtl #dolly {
+		#tencho,
+		.rtl #tencho {
 			float: none;
 			padding-left: 0;
 			padding-right: 0;
@@ -97,4 +80,4 @@ function dolly_css() {
 	";
 }
 
-add_action( 'admin_head', 'dolly_css' );
+add_action( 'admin_head', 'tencho_css' );
