ee3a252b3f61f86bdde63271805788fbdc3889ab
mt8 <info@mt8.biz>
2021-08-20 20:41:11 +0900

    change tencho words to const

--- includes/Core.php
+++ includes/Core.php
@@ -7,6 +7,17 @@ namespace mt8\Hello_Tencho;
  */
 class Core {
 
+	public const TENCHO_WORDS = 'Hi Tencho, I\'m Tencho and I\'m always selling mangoes.
+Do you like red mangoes? Or do you like green mangoes?
+Mangoes can only be harvested in the summer.
+I take good care of the mangoes I harvest.
+Do not put the mangoes in the refrigerator right away.
+The ripening process is very important for mangoes.
+When they change color and smell sweet, it is a sign that they are fully ripe.
+If you suddenly put them in the refrigerator, the ripening process will stop.
+After eating a delicious mango, try planting the seeds.
+Ah, mangoes. Ah, mangoes.';
+
 	private static $singleton;
 
 	/**
@@ -59,17 +70,7 @@ class Core {
 	 * @return string Tencho words.
 	 */
 	public function get_words() {
-		return "Hi Tencho, I'm Tencho and I'm always selling mangoes.
-Do you like red mangoes? Or do you like green mangoes?
-Mangoes can only be harvested in the summer.
-I take good care of the mangoes I harvest.
-Do not put the mangoes in the refrigerator right away.
-The ripening process is very important for mangoes.
-When they change color and smell sweet, it is a sign that they are fully ripe.
-If you suddenly put them in the refrigerator, the ripening process will stop.
-After eating a delicious mango, try planting the seeds.
-Ah, mangoes. Ah, mangoes.";
-
+		return self::TENCHO_WORDS;
 	}
 
 	/**
