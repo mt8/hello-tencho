0fde3ee4a0a4b58f8f682eb2e6372f24a8b065ce
mt8 <info@mt8.biz>
2021-08-21 19:13:27 +0900

    add front e2e test

--- tests/e2e/specs/hello.test.js
+++ tests/e2e/specs/hello.test.js
@@ -1,9 +1,20 @@
-import { visitAdminPage } from '@wordpress/e2e-test-utils';
+import { visitAdminPage,createURL } from '@wordpress/e2e-test-utils';
 
 describe( 'Hello Tencho', () => {
+
 	it('Exists Tencho words on admin page', async () => {
 		await visitAdminPage( '/' );
-		const text = await page.$eval('#tencho', item => item.textContent);
-		expect( text.length ).not.toEqual( 0 );
+		const adminText = await page.$eval('#tencho', item => item.textContent);
+		expect( adminText.length ).not.toEqual( 0 );
 	});
+
+	it('Not exists Tencho words on front page', async () => {
+		await page.goto(createURL('/'));
+		let frontText = await page.evaluate(() => {
+			let el = document.querySelector("#tencho");
+			return el ? el.innerText : "";
+		})
+		expect( frontText.length ).toEqual( 0 );
+	});
+
 } );
