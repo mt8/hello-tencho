92773b01bbb2d92ffc68cecc28327a8ce8cc86fb
mt8 <info@mt8.biz>
2023-01-14 22:22:52 +0900

    fix test(s)

--- includes/WP_CLI.php
+++ includes/WP_CLI.php
@@ -2,16 +2,16 @@
 
 namespace mt8\Hello_Tencho;
 
-if ( class_exists( '\WP_CLI_Command' ) ) :
-/**
- * Tencho CLI
- */
-class WP_CLI extends \WP_CLI_Command {
+if ( class_exists( '\WP_CLI_Command' ) ) {
+	/**
+	 * Tencho CLI
+	 */
+	class WP_CLI extends \WP_CLI_Command {
 
-	public function hello() {
-		$cli = \WP_CLI::class;
-		$cli::line( Core::get_instance()->get_chosen_words() );
+		public function hello() {
+			$cli = \WP_CLI::class;
+			$cli::line( Core::get_instance()->get_chosen_words() );
+		}
+
 	}
-
 }
-endif;
